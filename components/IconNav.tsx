00a01178e108b72beae33e8ced2977d2ec2ea31d
Phoenix <ayomikuntemitope.araoye@gmail.com>
2024-01-02 23:18:39 +0200

    completed icon nav

--- components/IconNav.tsx
+++ components/IconNav.tsx
@@ -10,7 +10,7 @@ function IconNav() {
   const router = useRouter();
 
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max mt-auto  xl:left-[2%] z-50  w-full xl:w-16 xl:max-w-md xl:h-screen">
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max mt-auto  xl:left-[2%] z-50  w-full xl:w-16 xl:max-w-md xl:h-screen bottom-0">
       <div className="dark:text-white flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl bg-black/20 dark:bg-white/20 xl:rounded-full">
         {navData.map((link) => {
           return (
