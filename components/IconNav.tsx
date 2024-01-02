6207b30b9af479ed9ac47e02dafd7dd671d70575
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-01-02 16:33:18 +0100

    Update IconNav.tsx

--- components/IconNav.tsx
+++ components/IconNav.tsx
@@ -10,7 +10,7 @@ function IconNav() {
   const router = useRouter();
 
   return (
-    <nav className="flex flex-col items-center xs:justify-center gap-y-4 fixed h-max mt-auto buttom-0 xl:left-right-[2% z-50  w-full xl:w-16 xl:max-w-max bg-lightBlue p-20 ">
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max mt-auto buttom-0 top-0 xl:left-[2%] z-50  w-full xl:w-16 xl:max-w-md xl:h-screen ">
       <div className="dark:text-white flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 bg-blue xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full ">
         {navData.map((link, Index) => {
           return (
