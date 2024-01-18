8b13e6f859911a00e9126f5d7a21e070cc9d72b5
Phoenix <ayomikuntemitope.araoye@gmail.com>
2024-01-19 00:20:23 +0200

    addd page wrapper to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AboutMe from "@/components/About";
+import PageWrapper from "@/components/Wrapper";
 
 const page = () => {
   return (
-    <>
+    <PageWrapper>
       <AboutMe />
-    </>
+    </PageWrapper>
   );
 };
 
