b2c37d3ca2ab7bb3c095c776cb9d845d9a0fbe29
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-03-07 11:29:29 +0000

    update

--- components/IconNav.tsx
+++ components/IconNav.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { navData } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import cn from "@/utils/twcx";
+import { motion, useAnimation } from "framer-motion";
 
 function IconNav() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max mt-auto  xl:left-[2%] z-50  w-full xl:w-16 xl:max-w-md xl:h-screen bottom-0">
-      <div className=" flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl bg-AAtertiary xl:rounded-full">
+    <motion.nav
+      initial={{ y: "100%" }}
+      animate={{ y: "0%" }}
+      transition={{ y: { delay: 11, duration: 0.5 } }}
+      className={cn(
+        "flex flex-col items-center xl:justify-center gap-y-4 fixed h-max mt-auto xl:left-[2%] z-50 w-full xl:w-16 xl:max-w-md xl:h-screen bottom-0"
+      )}
+    >
+      <motion.div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl bg-AAtertiary xl:rounded-full">
         {navData.map((link) => {
           return (
             <Link
@@ -37,8 +45,8 @@ function IconNav() {
             </Link>
           );
         })}
-      </div>
-    </nav>
+      </motion.div>
+    </motion.nav>
   );
 }
 
